e73c20123ff5b39ffa836d4d910b9b26056edac7
Shivam Shrivastav <shivam23711@gmail.com>
2025-06-18 18:52:05 +0530

    Learning MySQL and Database

---
+++ src/main/java/com/sql/SqlLearningApplication.java
@@ -0,0 +1,13 @@
+package com.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlLearningApplication.class, args);
+	}
+
+}
